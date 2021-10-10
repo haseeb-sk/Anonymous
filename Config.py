c17808690124b1e0f7f44b9d1c92511148fa0d59
haseeb-sk <92195975+haseeb-sk@users.noreply.github.com>
2021-10-10 09:02:16 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -10,7 +10,7 @@ if ENVIRONMENT:
     API_HASH = os.environ.get('API_HASH', None)
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
 else:
-    # Fill the Values
-    API_ID = 0
-    API_HASH = ""
-    BOT_TOKEN = ""
+    # Fill the Values 
+    API_ID = "2774426"
+    API_HASH = "e583437e972d1f801824a41d7aff65e7"
+    BOT_TOKEN = "2039163053:AAH_vRb_T2Hg0-RR_9mNWdthPvVMkwUnOmI"
